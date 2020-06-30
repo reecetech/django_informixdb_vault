18505292b57c21f8688f662ff93da41db647a03d
Phil Jay <phil.jay@reece.com.au>
2020-06-30 10:10:22 +1000

    Add vault to docker-compose, params to testing

--- test/conftest.py
+++ test/conftest.py
@@ -1,3 +1,4 @@
+import hvac
 import pytest
 from django.conf import settings
 
@@ -16,6 +17,9 @@ def pytest_configure():
                 "ENGINE": "django_informixdb_vault",
                 "SERVER": "informix",
                 "NAME": "adapter",
+                "VAULT_ADDR": "http://vault:8200",
+                "VAULT_TOKEN": "test-token",
+                "VAULT_PATH": "kv/ifx/creds",
             },
         },
     )
@@ -24,3 +28,17 @@ def pytest_configure():
 @pytest.fixture(autouse=True)
 def configure_caplog(caplog):
     caplog.set_level("INFO")
+
+@pytest.fixture(scope='module', autouse=True)
+def ifx_secret():
+    client = hvac.Client(url='http://vault:8200')
+    client.token = 'test-token'
+    if not client.is_authenticated():
+        raise RuntimeError('Dev vault not working as expected')
+    client.secrets.kv.v2.create_or_update_secret(
+        path='kv/ifx/creds',
+        secret={
+            'username': 'informix',
+            'password': 'in4mix',
+        }
+    )
