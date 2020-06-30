2060676029bccf69fb9fc0c629323cfb2af01721
Phil Jay <phil.jay@reece.com.au>
2020-06-30 16:54:49 +1000

    Cleanup after test

--- test-in-docker.sh
+++ test-in-docker.sh
@@ -4,6 +4,23 @@ set -euo pipefail
 export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
 
-docker-compose -p ifxvault build
+export uniq_test_id="${uniq_test_id:-djifxvault}"
 
-docker-compose -p ifxvault run test-runner tox
+cleanup() {
+    echo "--- cleaning up: previous code ${?}"
+    docker-compose -p "${uniq_test_id}" down --volumes --remove-orphans
+}
+trap cleanup EXIT
+
+echo "--- building docker images"
+docker-compose -p "${uniq_test_id}" build
+
+echo "--- starting dependencies"
+docker-compose -p "${uniq_test_id}" up -d db
+docker-compose -p "${uniq_test_id}" up -d vault
+
+echo "--- waiting for dependencies"
+docker-compose -p "${uniq_test_id}" run test-runner /usr/local/bin/wait-for-deps.sh
+
+echo "--- running tests"
+docker-compose -p "${uniq_test_id}" run test-runner tox
