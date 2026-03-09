cb186417de77e0b3c00f6b8b9fd52ea7e87596d5
Fran Sy <fran.sy@reece.com.au>
2026-03-09 14:42:02 +0800

    update test file

--- test/test_base.py
+++ test/test_base.py
@@ -1,10 +1,65 @@
+"""Tests for django_informix_vault/base.py
+"""
+import random
+import time
+
+import pytest
+
+from django.db import OperationalError
+from django_informixdb_vault.base import DatabaseWrapper as VaultDatabaseWrapper
+
+from .thread_utils import PropagatingThread
+
 from datetime import datetime
 from unittest.mock import MagicMock, mock_open
 
-import pytest
-from django.db import OperationalError
 
-from django_informixdb_vault.base import DatabaseWrapper
+@pytest.fixture
+def db_config():
+    return {
+        "ENGINE": "django_informixdb_vault",
+        "SERVER": "informix",
+        "NAME": "sysmaster",
+        "VAULT_ADDR": "http://vault:8200",
+        "VAULT_TOKEN": "test-token",
+        "VAULT_PATH": "kv/ifx/creds"
+    }
+
+
+def test_get_connection_params_multiple_threads(db_config, mocker):
+    """Test to catch thread safety issues with updating the settings_dict
+
+    django_informix_vault/base.py's get_connection_params method
+    modifies self.settings_dict from django.db.backends.base.BaseDatabaseWrapper
+    """
+    num_threads = 10
+    db_conn = VaultDatabaseWrapper(db_config)
+    conn_params = db_conn.get_connection_params()
+
+    def slow_get_conn_params():
+        # Sleep for pseudo-random number of seconds in the range [0.0, 1.0)
+        time.sleep(random.random())
+        return conn_params
+
+    # Patch the non-vault parent class's get_connection_params method:
+    mocker.patch(
+        "django_informixdb.base.DatabaseWrapper.get_connection_params",
+        side_effect=slow_get_conn_params,
+    )
+
+    def get_conn_params_worker():
+        db_conn.get_connection_params()
+
+    threads = []
+    for tnum in range(num_threads):
+        threads.append(PropagatingThread(target=get_conn_params_worker))
+        threads[tnum].start()
+
+    # Because we're using PropagatingThread, calling join on each thread
+    # will raise any exception which was raised within the thread,
+    # e.g. KeyError: 'USER'
+    for tnum in range(num_threads):
+        threads[tnum].join()
 
 
 @pytest.fixture
@@ -22,7 +77,7 @@ def settings_dict(mocker):
 
 @pytest.fixture
 def db_wrapper(settings_dict):
-    return DatabaseWrapper(settings_dict)
+    return VaultDatabaseWrapper(settings_dict)
 
 
 def test_get_vault_uri(db_wrapper):
@@ -48,7 +103,7 @@ def test_get_maximum_credential_lifetime_default(db_wrapper):
 def test_auth_via_k8s(mocker, db_wrapper):
     mock_client = MagicMock()
     mocker.patch('builtins.open', mock_open(read_data='test-jwt'))
-    mocker.patch('os.access', return_value=True)  
+    mocker.patch('os.access', return_value=True)
 
     db_wrapper._auth_via_k8s(mock_client)
 
@@ -65,8 +120,8 @@ def test_get_authenticated_client_with_k8s(mocker, db_wrapper):
     mocker.patch(
         'builtins.open',
         mock_open(read_data='mocked-jwt-content')
-    )  
-    mocker.patch('os.access', return_value=True)  
+    )
+    mocker.patch('os.access', return_value=True)
     mock_client.is_authenticated.return_value = True
 
     client = db_wrapper.get_authenticated_client()
@@ -78,7 +133,7 @@ def test_get_authenticated_client_with_k8s(mocker, db_wrapper):
 def test_get_authenticated_client_with_token(mocker, settings_dict):
     settings_dict.pop('VAULT_K8S_ROLE')
     settings_dict['VAULT_TOKEN'] = 'test-token'
-    db_wrapper = DatabaseWrapper(settings_dict)
+    db_wrapper = VaultDatabaseWrapper(settings_dict)
 
     mock_client = MagicMock()
     mocker.patch('hvac.Client', return_value=mock_client)
@@ -98,8 +153,8 @@ def test_get_authenticated_client_raises_exception(
     mocker.patch(
         'builtins.open',
         mock_open(read_data='mocked-jwt-content')
-    )  
-    mocker.patch('os.access', return_value=True)  
+    )
+    mocker.patch('os.access', return_value=True)
     mock_client.is_authenticated.return_value = False
 
     with pytest.raises(OperationalError):
@@ -115,8 +170,8 @@ def test_get_credentials_from_vault(mocker, db_wrapper):
     mocker.patch(
         'builtins.open',
         mock_open(read_data='mocked-jwt-content')
-    )  
-    mocker.patch('os.access', return_value=True)  
+    )
+    mocker.patch('os.access', return_value=True)
 
     username, password = db_wrapper.get_credentials_from_vault()
 
@@ -132,8 +187,8 @@ def test_get_credentials_from_vault_raises_exception(mocker, db_wrapper):
     mocker.patch(
         'builtins.open',
         mock_open(read_data='mocked-jwt-content')
-    )  
-    mocker.patch('os.access', return_value=True)  
+    )
+    mocker.patch('os.access', return_value=True)
 
     with pytest.raises(OperationalError):
         db_wrapper.get_credentials_from_vault()
